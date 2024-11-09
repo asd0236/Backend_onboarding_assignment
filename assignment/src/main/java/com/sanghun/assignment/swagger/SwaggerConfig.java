5b76df7765f739a3a82af621238bb2d419ff4eb3
sanghun <tkdgns0236@gmail.com>
2024-11-09 20:20:26 +0900

    feat: Swagger UI 적용

---
+++ assignment/src/main/java/com/sanghun/assignment/swagger/SwaggerConfig.java
@@ -0,0 +1,34 @@
+package com.sanghun.assignment.swagger;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Backend_onboarding_assignment").description("Backend_onboarding_assignment API Swaggger UI 입니다.").version("1.0.0"))
+                .components(new Components()
+                        .addSecuritySchemes("bearer-jwt", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")))
+                .addSecurityItem(new SecurityRequirement().addList("bearer-jwt"));
+    }
+
+    @Bean
+    public GroupedOpenApi customApi() {
+        return GroupedOpenApi.builder()
+                .group("api")
+                .pathsToMatch("/**")
+                .build();
+    }
+
+}
